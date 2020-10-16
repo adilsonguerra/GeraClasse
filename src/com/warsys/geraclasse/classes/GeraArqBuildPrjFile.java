29601a3df1fcbee9c0c1f22946be8a5fc155b6cb
Adilson Guerra <adilsonguerra@bol.com.br>
2020-10-16 10:30:19 +0100

    Update GeraArqBuildPrjFile.java

--- src/com/warsys/geraclasse/classes/GeraArqBuildPrjFile.java
+++ src/com/warsys/geraclasse/classes/GeraArqBuildPrjFile.java
@@ -14,13 +14,15 @@ public class GeraArqBuildPrjFile {
             String empresa =  nomeEmpSist.replace('.','/');
             Arquivo a = new Arquivo("");
             a.criar("build_prj.xml");
-
-            a.escrever("<?xml version="+ '\"'+"1.0"+ '\"'+" encoding="+ '\"'+"iso-8859-1"+ '\"'+" ?>",0);
+		 
+		 "+'\"'+
+                  +'\"'+"
+            a.escrever("<?xml version="+'\"'+"1.0"+'\"'+" encoding="+ '\"'+"iso-8859-1"+ '\"'+" ?>",0);
             a.escrever("<!-- Careega toda a estrutura para o tomcat -->",0);
             a.escrever("<project default="+ '\"'+"carrega_tomcat"+ '\"'+" basedir="+ '\"'+"."+ '\"'+" >",0);
             a.escrever("",0);
             a.escrever("   <property name="+ '\"'+"app.home"+ '\"'+" value="+ '\"'+"C:/"+empresa+ '\"'+" />",0);
-            a.escrever("   <property name="+ '\"'+"app.name"+ '\"'+" value="+ '\"'+nomeEmpSist.substring(nomeEmpSist.lastIndexOf(".")+1) + '\"'+" />",0);
+            a.escrever("   <property name="+ '\"'+"app.name"+ '\"'+" value="+ '\"'+nomeEmpSist.substring(nomeEmpSist.lastIndexOf(".")+1) +'\"'+" />",0);
             a.escrever("   <property name="+ '\"'+"tomcat.home"+ '\"'+" value="+ '\"'+ tomcatHome + '\"'+" />",0);
             a.escrever("   <property name="+ '\"'+"deploy.home"+ '\"'+" value="+ '\"'+"${tomcat.home}/webapps/${app.name}"+ '\"'+" />",0);
             a.escrever("",0);  
@@ -31,6 +33,18 @@ public class GeraArqBuildPrjFile {
             a.escrever("      <delete file="+ '\"'+"${deploy.home}/build_prj.xml"+ '\"'+"/>",0);         
             a.escrever("      <copy todir="+ '\"'+"${deploy.home}/WEB-INF/classes"+ '\"'+">  <fileset dir ="+ '\"'+"${app.home}/WebContent/WEB-INF/classes"+ '\"'+"/> </copy>",0);
             a.escrever("   </target>",0);
+	    a.escrever("",0); 
+   	    a.escrever("   <target name="+'\"'+"compile"+'\"'+" description="+'\"'+"Compile main source tree java files"+'\"'+">",0);
+            a.escrever("      <javac srcdir="+'\"'+"${app.home}/src"+'\"'+"destdir="+'\"'+"${app.home}/WebContent/WEB-INF/classes"+'\"'+",0); 
+      	    a.escrever("      debug="+'\"'+"true"+'\"'+",0);
+	    a.escrever("      deprecation="+'\"'+"false"+'\"'+",0);
+      	    a.escrever("      optimize="+'\"'+"true"+'\"'+",0); 
+      	    a.escrever("      failonerror="+'\"'+"true"+'\"'+">",0);
+            a.escrever("      <classpath>",0);
+            a.escrever("         <pathelement location="+'\"'+"${tomcat.home}/lib/servlet-api.jar"+'\"'+" />",0);
+            a.escrever("      </classpath>",0);
+            a.escrever("      </javac>",0);	   	
+            a.escrever("   </target>",0);		 
             a.escrever("</project>",0);
             a.escrever("",0);
             a.fechar();
